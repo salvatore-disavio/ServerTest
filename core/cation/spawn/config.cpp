7c210fcfa9efaa062566315e094836bcc303e44c
sasy992 <30809172+sasy992@users.noreply.github.com>
2018-02-18 18:15:06 +0100

    Add files via upload

---
+++ core/cation/spawn/config.cpp
@@ -0,0 +1,14 @@
+/*
+    File: config.cpp
+    Author: B4v4r!4n_Str!k3r (julianbauer@cationstudio.com)
+    Licence: THIS FILE AND EXTRACTS OF IT IS THE MINDSET OF CATIONSTUDIO
+             AND ONLY AUTHORIZED PEOPLE/SERVERS ARE ALLOWED TO USE IT.
+    Description: Master config for crafting system. 
+*/
+#include "defines.hpp"
+#include "spawnSelection.hpp"
+class Cation_Spawn {
+
+    camDistance = 300; //Höhe der Kamera
+    
+};
